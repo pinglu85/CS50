d7ae9766d3705c7d601fa47900012ee252b081b3
pinglu85 <luping8576@hotmail.com>
2020-07-03 23:23:48 +0200

    Initial commit

---
+++ week_1_c/parity.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) 
+{
+    // Prompt user for integer
+    int n = get_int("n: ");
+    
+    if ( n % 2 == 0) 
+    {
+        printf("even\n");
+    }
+    else 
+    {
+        printf("odd\n");   
+    }
+}
