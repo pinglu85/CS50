f345670600b715ab52960d91a07b13f6d930b548
pinglu85 <luping8576@hotmail.com>
2021-06-22 05:33:21 +0200

    fix(helpers.py): deprecated IEX Cloud API base url

--- pset8/web/finance/helpers.py
+++ pset8/web/finance/helpers.py
@@ -45,7 +45,7 @@ def lookup(symbol):
     try:
         api_key = os.environ.get("API_KEY")
         response = requests.get(
-            f"https://cloud-sse.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}")
+            f"https://cloud.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}")
         response.raise_for_status()
     except requests.RequestException:
         return None
